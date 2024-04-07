9e0d07820841d76de62843d31c30575ff2d2fbbf
peter-lyr <llydrp.ldp@gmail.com>
2024-04-07 23:16:43 +0800

    function M.concant_info(prefix, info)

--- lua/dp_base.lua
+++ lua/dp_base.lua
@@ -117,4 +117,36 @@ function M.is_dir(file)
   return nil
 end
 
+function M.concant_info(prefix, info)
+  --[[ use like this:
+     [ function M.find_files_in_current_project_git_modified(params, ...)
+     [   if ... then return M.concant_info(..., debug.getinfo(1)['name']) end
+     [   -- else do with params
+     [ end
+     ]]
+  prefix = tostring(prefix)
+  if #prefix == 0 then
+    prefix = M.lua
+  end
+  return prefix .. ': ' .. vim.fn.join(vim.fn.split(info, '_'))
+end
+
+function M.getlua(luafile)
+  local loaded = string.match(luafile, '.+lua/(.+)%.lua')
+  if not loaded then
+    return ''
+  end
+  loaded = string.gsub(loaded, '/', '.')
+  return loaded
+end
+
+function M.getsource(luafile)
+  return M.rep(vim.fn.trim(luafile, '@'))
+end
+
+function M.rep(content)
+  content = string.gsub(content, '/', '\\')
+  return vim.fn.tolower(content)
+end
+
 return M
