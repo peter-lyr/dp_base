d3dc190a42a021b1908e56663d677d0f814c5084
peter-lyr <llydrp.ldp@gmail.com>
2024-04-08 10:09:29 +0800

    local system_cmd = require 'dp_base.system_cmd'

--- lua/dp_base/init.lua
+++ lua/dp_base/init.lua
@@ -3,46 +3,13 @@
 
 local M = {}
 
-local dp_asyncrun = require 'dp_asyncrun'
+local system_cmd = require 'dp_base.system_cmd'
 
-function M.system_run(way, str_format, ...)
-  if type(str_format) == 'table' then
-    str_format = vim.fn.join(str_format, ' && ')
-  end
-  local cmd = string.format(str_format, ...)
-  if way == 'start' then
-    cmd = string.format([[silent !start cmd /c "%s"]], cmd)
-    vim.cmd(cmd)
-  elseif way == 'start silent' then
-    cmd = string.format([[silent !start /b /min cmd /c "%s"]], cmd)
-    vim.cmd(cmd)
-  elseif way == 'asyncrun' then
-    vim.cmd 'AsyncStop'
-    cmd = string.format('AsyncRun %s', cmd)
-    if vim.g.asyncrun_status == 'running' then
-      M.timer_temp = vim.fn.timer_start(10, function()
-        if vim.g.asyncrun_status ~= 'running' then
-          pcall(vim.fn.timer_stop, M.timer_temp)
-          dp_asyncrun.done_default()
-          vim.cmd(cmd)
-        end
-      end, { ['repeat'] = -1, })
-    else
-      vim.cmd(cmd)
-      dp_asyncrun.done_default()
-    end
-  elseif way == 'term' then
-    cmd = string.format('wincmd s|term %s', cmd)
-    vim.cmd(cmd)
-  else
-    return
-  end
-end
+M.system_run = system_cmd.system_run
+M.system_run_histadd = system_cmd.system_run_histadd
 
-function M.system_run_histadd(way, str_format, ...)
-  M.system_run(way, str_format, ...)
-  vim.fn.histadd(':', cmd)
-end
+M.cmd = system_cmd.cmd
+M.cmd_histadd = system_cmd.cmd_histadd
 
 function M.lazy_map(tbls)
   for _, tbl in ipairs(tbls) do
@@ -61,16 +28,6 @@ function M.lazy_map(tbls)
       vim.keymap.set(tbl['mode'], lhs, tbl[2], opt)
     end
   end
-end
-
-function M.cmd(str_format, ...)
-  local cmd = string.format(str_format, ...)
-  vim.cmd(cmd)
-  return cmd
-end
-
-function M.cmd_histadd(str_format, ...)
-  vim.fn.histadd(':', M.cmd(str_format, ...))
 end
 
 function M.aucmd(event, desc, opts)
---
+++ lua/dp_base/system_cmd.lua
@@ -0,0 +1,57 @@
+-- Copyright (c) 2024 liudepei. All Rights Reserved.
+-- create at 2024/04/08 10:06:07 Monday
+
+local M = {}
+
+local dp_asyncrun = require 'dp_asyncrun'
+
+function M.system_run(way, str_format, ...)
+  if type(str_format) == 'table' then
+    str_format = vim.fn.join(str_format, ' && ')
+  end
+  local cmd = string.format(str_format, ...)
+  if way == 'start' then
+    cmd = string.format([[silent !start cmd /c "%s"]], cmd)
+    vim.cmd(cmd)
+  elseif way == 'start silent' then
+    cmd = string.format([[silent !start /b /min cmd /c "%s"]], cmd)
+    vim.cmd(cmd)
+  elseif way == 'asyncrun' then
+    vim.cmd 'AsyncStop'
+    cmd = string.format('AsyncRun %s', cmd)
+    if vim.g.asyncrun_status == 'running' then
+      M.timer_temp = vim.fn.timer_start(10, function()
+        if vim.g.asyncrun_status ~= 'running' then
+          pcall(vim.fn.timer_stop, M.timer_temp)
+          dp_asyncrun.done_default()
+          vim.cmd(cmd)
+        end
+      end, { ['repeat'] = -1, })
+    else
+      vim.cmd(cmd)
+      dp_asyncrun.done_default()
+    end
+  elseif way == 'term' then
+    cmd = string.format('wincmd s|term %s', cmd)
+    vim.cmd(cmd)
+  else
+    return
+  end
+end
+
+function M.system_run_histadd(way, str_format, ...)
+  M.system_run(way, str_format, ...)
+  vim.fn.histadd(':', cmd)
+end
+
+function M.cmd(str_format, ...)
+  local cmd = string.format(str_format, ...)
+  vim.cmd(cmd)
+  return cmd
+end
+
+function M.cmd_histadd(str_format, ...)
+  vim.fn.histadd(':', M.cmd(str_format, ...))
+end
+
+return M
