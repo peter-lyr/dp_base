b0008cad8a3fa1bbd24bcd1dc5e9cd2726cdddee
peter-lyr <llydrp.ldp@gmail.com>
2024-04-21 23:16:50 +0800

    M.chars 26

--- lua/dp_base.lua
+++ lua/dp_base.lua
@@ -18,32 +18,12 @@ M.yanking = nil
 M.temp_maps = {}
 
 M.chars = {
-  'A', 'a',
-  'B', 'b',
-  'C', 'c',
-  'D', 'd',
-  'E', 'e',
-  'F', 'f',
-  'G', 'g',
-  'H', 'h',
-  'I', 'i',
-  'J', 'j',
-  'K', 'k',
-  'L', 'l',
-  'M', 'm',
-  'N', 'n',
-  'O', 'o',
-  'P', 'p',
-  'Q', 'q',
-  'R', 'r',
-  'S', 's',
-  'T', 't',
-  'U', 'u',
-  'V', 'v',
-  'W', 'w',
-  'X', 'x',
-  'Y', 'y',
-  'Z', 'z',
+  'a', 'b', 'c', 'd', 'e',
+  'f', 'g', 'h', 'i', 'j',
+  'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y',
+  'z',
 }
 
 function M.check_plugins(plugins)
